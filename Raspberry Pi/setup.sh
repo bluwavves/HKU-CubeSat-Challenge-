68550d521002a617747e2d006e94de503212d32c
Soomin Kim <93189187+bluwavves@users.noreply.github.com>
2022-01-06 21:35:48 +0800

    Update setup.sh

--- Raspberry Pi/setup.sh
+++ Raspberry Pi/setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-sudo su
+sudo su #change to root user
+#Packages 
 apt-get update -y && apt-get upgrade -y
-apt-get python3 -y
+apt-get python3 -y 
 apt-get pip -y
-pip install PiCamera
+#Python libraries 
+pip install PiCamera 
 pip install Pillow
 pip install scipy
+#back to user pi 
 su - pi
+mkdir 'Images'
